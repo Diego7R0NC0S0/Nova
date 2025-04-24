d2c3afa20df572501076d515c92d8584d865f3d3
Diego7R0NC0S0 <diego.troncoso.lopez@gmail.com>
2025-04-24 06:25:28 -0400

    Primer commit del proyecto Nova

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Hero from './componets/Hero'
+import About from './componets/About'
+import Navbar from './componets/Navbar'
+import Features from './componets/Features'
+import Story from './componets/Story'
+import Contact from './componets/Contact'
+import Footer from './componets/Footer'
+
+const App = () => {
+  return (
+    <main className='relative min-h-screen w-screen overflow-hidden '>
+      <h1>2:21:32</h1>
+      <Navbar/>
+      <Hero/>
+      <About/>
+      <Features/>
+      <Story/>
+      <Contact/>
+      <Footer/>
+    </main>
+  )
+}
+
+export default App
